840a3035430563d5f9ee7fb5531462ac652821d4
liuqunshan <liuqunshan@hisense.com>
2017-06-02 19:02:55 +0800

    演示如何使用java插件。

---
+++ test_java/Test.java
@@ -0,0 +1,13 @@
+package test_java;
+
+/**
+ * @Author: liuqunshan@hisense.com
+ * @Date: 2017-6-2
+ * @Description: TODO
+ */
+public class Test {
+
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
+}
